a1391935d411e102c9754163fbb2b01d8986bd9f
0xddaa <0xddaa@gmail.com>
2017-04-27 01:33:59 +0800

    add searchmem and alias

--- pwndbg/commands/peda.py
+++ pwndbg/commands/peda.py
@@ -27,3 +27,36 @@ def getfile():
 @pwndbg.commands.OnlyWhenRunning
 def getpid():
     peda.getpid()
+
+@pwndbg.commands.Command
+@pwndbg.commands.OnlyWhenRunning
+def searchmem(*arg):
+    """
+    Search for a pattern in memory; support regex search
+    Usage:
+        MYNAME pattern start end
+        MYNAME pattern mapname
+    """
+    (pattern, start, end) = normalize_argv(arg, 3)
+    (pattern, mapname) = normalize_argv(arg, 2)
+    if pattern is None:
+        raise TypeError
+
+    pattern = arg[0]
+    result = []
+    if end is None and to_int(mapname):
+        vmrange = peda.get_vmrange(mapname)
+        if vmrange:
+            (start, end, _, _) = vmrange
+
+    if end is None:
+        msg("Searching for %s in: %s ranges" % (repr(pattern), mapname))
+        result = peda.searchmem_by_range(mapname, pattern)
+    else:
+        msg("Searching for %s in range: 0x%x - 0x%x" % (repr(pattern), start, end))
+        result = peda.searchmem(start, end, pattern)
+
+    text = peda.format_search_result(result)
+    pager(text)
+
+Alias("find", "searchmem") # override gdb find command
