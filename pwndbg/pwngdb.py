b231f09be400348ff237072cc7513187903a10e7
0xddaa <0xddaa@gmail.com>
2019-04-12 07:41:14 +0000

    refactor pwngdb

--- pwndbg/pwngdb.py
+++ pwndbg/pwngdb.py
@@ -27,62 +27,56 @@ def to_int(val):
         return val
 
 def procmap():
-    data = gdb.execute("info proc exe", to_string=True)
-    pid = re.search("process.*", data)
-    if pid :
-        pid = pid.group().split()[1]
-        with open("/proc/{}/maps".format(pid), "r") as maps:
-            return maps.read()
-    else :
-        return "error"
+    with open("/proc/{}/maps".format(pwndbg.proc.pid), "r") as maps:
+        return maps.read()
 
 def getlibcbase():
     data = re.search(".*libc.*\.so", procmap())
-    if data :
-        libcaddr = data.group().split("-")[0]
-        gdb.execute("set $libc={}".format(hex(int(libcaddr, 16))))
-        return int(libcaddr, 16)
-    else :
+    if not data:
         return 0
+
+    libcaddr = data.group().split("-")[0]
+    gdb.execute("set $libc={}".format(hex(int(libcaddr, 16))))
+    return int(libcaddr, 16)
 
 def getheapbase():
     data = re.search(".*heap\]", procmap())
-    if data :
-        heapbase = data.group().split("-")[0]
-        gdb.execute("set $heap={}".format(hex(int(heapbase, 16))))
-        return int(heapbase, 16)
-    else :
+    if not data:
         return 0
+
+    heapbase = data.group().split("-")[0]
+    gdb.execute("set $heap={}".format(hex(int(heapbase, 16))))
+    return int(heapbase, 16)
 
 def ldbase():
     data = re.search(".*ld.*\.so", procmap())
-    if data :
-        ldaddr = data.group().split("-")[0]
-        gdb.execute("set $ld={}".format(hex(int(ldaddr, 16))))
-        return int(ldaddr, 16)
-    else :
+    if not data:
         return 0
+
+    ldaddr = data.group().split("-")[0]
+    gdb.execute("set $ld={}".format(hex(int(ldaddr, 16))))
+    return int(ldaddr, 16)
 
 def codeaddr(): # ret (start, end)
     pat = ".*" + pwndbg.proc.exe
     data = re.findall(pat, procmap())
-    if data :
-        codebaseaddr = data[0].split("-")[0]
-        codeend = data[0].split("-")[1].split()[0]
-        gdb.execute("set $code={}".format(hex(int(codebaseaddr, 16))))
-        return (int(codebaseaddr, 16), int(codeend, 16))
-    else :
+    if not data:
         return (0, 0)
+
+    codebaseaddr = data[0].split("-")[0]
+    codeend = data[0].split("-")[1].split()[0]
+    gdb.execute("set $code={}".format(hex(int(codebaseaddr, 16))))
+    return (int(codebaseaddr, 16), int(codeend, 16))
 
 def gettls():
     arch = pwndbg.arch.current
 
-    if arch == "i386" :
+    if arch == "i386":
         vsysaddr = gdb.execute("info functions __kernel_vsyscall", to_string=True).split("\n")[-2].split()[0].strip()
         value = struct.pack("<L", int(vsysaddr, 16))
         sysinfo = [address for address in pwndbg.search.search(value)][0]
         return sysinfo - 0x10
-    elif arch == "x86-64" :
+    elif arch == "x86-64":
         gdb.execute("call arch_prctl(0x1003, $rsp-8)", to_string=True)
         data = gdb.execute("x/xg $rsp-8", to_string=True)
         return int(data.split(":")[1].strip(), 16)
@@ -95,15 +89,15 @@ def getoff(symbol):
 
     if isinstance(symbol, int):
         return symbol - libc
-    else :
-        try :
+    else:
+        try:
             data = gdb.execute("x/x " + symbol, to_string=True)
             if "No symbol" in data:
                 return -1
-            else :
+            else:
                 symaddr = int(re.search("0x.*[0-9a-f] ", data).group()[:-1], 16)
                 return symaddr - libc
-        except :
+        except:
             return -1
 
 def iscplus():
@@ -113,9 +107,9 @@ def searchcall(symbol):
     procname = pwndbg.proc.exe
     cmd = "objdump -d -M intel {} {}".format("--demangle" if iscplus() else "", procname)
     cmd += "| grep 'call.*{}@plt'".format(symbol)
-    try :
+    try:
         return subprocess.check_output(cmd, shell=True).decode("utf8").strip("\n")
-    except :
+    except:
         return -1
 
 def ispie():
